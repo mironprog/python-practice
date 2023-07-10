2669d0e31269499b015882210e76e623840bff98
mironprog <bmaresz90@gmail.com>
2023-07-11 00:20:19 +0200

    Learning about variables

---
+++ Basics/Variables/MultipleAssignment.py
@@ -0,0 +1,22 @@
+
+# name = "Jean d'Arc"
+# age = 19
+# hot = True
+
+name, age, hot = "Jean d'Arc", 19, True
+
+print(name)
+print(age)
+print(hot)
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
