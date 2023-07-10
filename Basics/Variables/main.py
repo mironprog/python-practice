f07de79d32569fff87297f788fa8e8c11173df1d
mironprog <bmaresz90@gmail.com>
2023-07-11 00:10:09 +0200

    Overview of the variables

---
+++ Basics/Variables/main.py
@@ -0,0 +1,26 @@
+# Strings
+
+first_name = "Mark"
+last_name = "Watney"
+full_name = first_name +" "+ last_name
+
+name = "Miron"
+print(type("Hello "+name))
+print("Hello "+full_name)
+
+age = 21
+age += 1
+print("Your age is: "+str(age))
+print(type(age))
+
+height = 250.5
+print("Your height is: "+str(height)+" cm")
+print(type(height))
+
+human = False
+print(human)
+print(type(human))
+print("Are you a human: "+str(human))
+
+
+
