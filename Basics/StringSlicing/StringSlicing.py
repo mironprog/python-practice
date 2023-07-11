0fe13b818d3df8b7672b3a3917865a03dc0e3164
mironprog <bmaresz90@gmail.com>
2023-07-12 01:26:31 +0200

    Learning string slicing

---
+++ Basics/StringSlicing/StringSlicing.py
@@ -0,0 +1,23 @@
+# slicing = create a substring by extracking elements from another string
+#               indexing[] or slice()
+#                [start:stop:step]
+
+#name = "Alan Turing"
+#
+#first_name = name[0:4:1]
+#last_name = name[5:11:1]
+#funky_name = name[::2]
+#reversed_name = name[::-1]
+#
+#print(first_name)
+#print(last_name)
+#print(funky_name)
+#print(reversed_name)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
