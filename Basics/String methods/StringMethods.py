4332d20ff30a68bc36e3b5f30bf563f8b4c44b98
mironprog <bmaresz90@gmail.com>
2023-07-11 23:10:53 +0200

    A few useful String methods

---
+++ Basics/String methods/StringMethods.py
@@ -0,0 +1,13 @@
+name = "Teller Ede"
+
+print(len(name))
+print(name.find("T"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("e"))
+print(name.replace("e", "o"))
+print(name*3)
+
