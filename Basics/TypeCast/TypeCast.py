b8658165f352317ef9f86cdfec037b2ead1b0464
mironprog <bmaresz90@gmail.com>
2023-07-11 23:20:25 +0200

    Basics of type casting

---
+++ Basics/TypeCast/TypeCast.py
@@ -0,0 +1,13 @@
+#type casting = convert the data type of a value to another data type
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #string
+
+x = float(x)
+y = str(y)
+z = int(z)
+
+print("X is: "+str(x))
+print("Y is: "+str(y))
+print(z*3)
